0f516f1e443846829eb4f63d472f721d90d796f7
Ravneet2511 <123626175+Ravneet2511@users.noreply.github.com>
2023-01-26 14:02:51 +0530

    Add files via upload

---
+++ CPP WORK/WEEK1_CPP3_RAVNEET SINGH_CIPHERSCHOOL/LEC#8/constructor.cpp
@@ -0,0 +1,43 @@
+#include<iostream>
+using namespace std;
+class student {
+    string passcode;
+    // int,char,string,float..
+    public :
+    string name;
+    int id;
+
+    student(){
+
+    }
+    student(string passcode, string name,int id){
+        this->name = name;
+        // age = a;
+        // i = id;
+       this->id = id;
+        this->passcode = passcode;
+    }
+
+    void into(){
+        cout<<"My name is "<< name <<", my id is "<<id<<" , passcode is "<<passcode<<endl;
+
+    }
+
+    void setpass(string s){
+        passcode = s;
+    }
+};
+int main(){
+    student s1;
+    student s2("10","mohit",1);
+    student s3;
+
+    s3 = s2;
+    
+    //s3+s2;
+    
+    s3.into();
+
+    
+    return 0;
+}
