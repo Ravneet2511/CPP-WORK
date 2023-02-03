f7800505b78ffc5d61aafbb017b51ab556d263be
Ravneet2511 <123626175+Ravneet2511@users.noreply.github.com>
2023-02-03 18:38:27 +0530

    Add files via upload

---
+++ WEEK2_CPP3_RAVNEET SINGH_CIPHERSCHOOL/LEC#19/LinkedListCycle.cpp
@@ -0,0 +1,25 @@
+/**
+* Defination for singly-linked list.
+* struct ListNode {
+*  		int val;
+*		ListNode *next;
+*		ListNode(int x) : val(x), next(NULL) {}
+*  };
+*/
+class Solution {
+	public:
+		bool hasCycle(ListNode *head) {
+			unordered_map<ListNode *,int>mp;
+			ListNode *t = head;
+			
+			while(t){
+				if(mp.count(t->next))
+					return 1;
+				mp[t]++;
+				t = t->next;
+			}
+			
+			return 0;
+		}
+};
+		
